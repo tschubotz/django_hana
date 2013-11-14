ea40079ef4c74433542fb22c4b1c49c3f74bc640
Tobias Schubotz <tobias.schubotz@gmail.com>
2013-11-14 17:26:49 +0100

    another attempt to fix connect problem

--- django_hana/database.py
+++ django_hana/database.py
@@ -5,8 +5,13 @@ from jaydebeapi import Connection as j_Connection
 # extend the Connection class by the method setautocommit used by django_hana
 class Connection(j_Connection):
 
-  def setautocommit(auto):
+  @classmethod
+  def from_j_connection(cls, j_connection):
+    return cls(j_connection.jconn, j_connection._converters)
+
+  def setautocommit(self, auto):
     self.jconn.setAutoCommit(auto)
 
 def connect(address, port, user, password):
-  return j_connect('com.sap.db.jdbc.Driver', ['jdbc:sap://{0}:{1}/'.format(address, port), user, password], 'ngdbc.jar')
+  j_connection = j_connect('com.sap.db.jdbc.Driver', ['jdbc:sap://{0}:{1}/'.format(address, port), user, password], 'ngdbc.jar')
+  return Connection.from_j_connection(j_connection)

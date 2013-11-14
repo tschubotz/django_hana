3c0b7bd5f7eb3d50f39bda5ee68ef69edc6d68d8
Tobias Schubotz <tobias.schubotz@gmail.com>
2013-11-14 17:17:47 +0100

    work on connect scope problem

--- django_hana/database.py
+++ django_hana/database.py
@@ -1,10 +1,12 @@
 from jaydebeapi import *
+from jaydebeapi import connect as j_connect
+from jaydebeapi import Connection as j_Connection
 
 # extend the Connection class by the method setautocommit used by django_hana
-class Connection(Connection):
+class Connection(j_Connection):
 
   def setautocommit(auto):
     self.jconn.setAutoCommit(auto)
 
 def connect(address, port, user, password):
-  return connect('com.sap.db.jdbc.Driver', ['jdbc:sap://{0}:{1}/'.format(address, port), user, password], 'ngdbc.jar')
+  return j_connect('com.sap.db.jdbc.Driver', ['jdbc:sap://{0}:{1}/'.format(address, port), user, password], 'ngdbc.jar')
